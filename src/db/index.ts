2ee7e6ae418fc892c3868ca87e16a51a61156e2c
Tushar Agrawal <tushar.code.inbox@gmail.com>
2024-06-23 14:11:15 +0530

    feat: mongo setup

---
+++ src/db/index.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+    );
+    console.log(
+      `\n☘️  MongoDB Connected! Db host: ${connectionInstance.connection.host}\n`
+    );
+  } catch (error) {
+    console.log("MongoDB connection error: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-import app from './app';
+import app from "./app";
+import connectDB from "./db";
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
-  /* eslint-enable no-console */
-});
+(async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      /* eslint-disable no-console */
+      console.log(`⚙️  Server is running on: http://localhost:${port}\n`);
+      /* eslint-enable no-console */
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+})();
