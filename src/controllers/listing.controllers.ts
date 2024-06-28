3c4132140ea089e57f7aacdbaa41fdff5adb944b
Tushar Agrawal <tushar.code.inbox@gmail.com>
2024-06-28 15:34:52 +0530

    feat: pagination on listing

--- src/controllers/listing.controllers.ts
+++ src/controllers/listing.controllers.ts
@@ -4,18 +4,56 @@ import ApiResponse from "../utils/ApiResponse";
 import ApiError from "../utils/ApiError";
 
 const getAllListings = async (req: Request, res: Response) => {
+  const page = parseInt(req.query.page as string);
+  const limit = parseInt(req.query.limit as string);
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  interface Data {
+    next?: {
+      page: number;
+      limit: number;
+    };
+    previous?: {
+      page: number;
+      limit: number;
+    };
+    listings?: any[];
+  }
+
+  const data: Data = {};
+
   try {
-    const listings = await Listing.find();
-    if (!listings) {
-      return res.status(500).json(new ApiError(409, "No listings", []));
+    const totalListings = await Listing.countDocuments();
+
+    if (endIndex < totalListings) {
+      data.next = {
+        page: page + 1,
+        limit: limit,
+      };
     }
+
+    if (startIndex > 0) {
+      data.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+
+    data.listings = await Listing.find().limit(limit).skip(startIndex);
+
+    if (!data.listings || data.listings.length === 0) {
+      return res.status(409).json(new ApiError(409, "No listings", []));
+    }
+
     return res
       .status(200)
-      .json(new ApiResponse(200, { listings }, "Data fetching successful"));
+      .json(new ApiResponse(200, data, "Data fetching successful"));
   } catch (error) {
     return res
       .status(500)
-      .json(new ApiError(409, "Error while fetching listings", [error]));
+      .json(new ApiError(500, "Error while fetching listings", [error]));
   }
 };
 
